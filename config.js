4d6128ace5a273482e14b41c85a749c01c6d16f2
Paige Hubbell <paigetech@gmail.com>
2014-09-18 11:53:13 -0700

    add email settings

--- config.js
+++ config.js
@@ -4,3 +4,13 @@ exports.db = 'http://127.0.0.1:5984/';
 
 // settings for CouchDB in cloud (i.e. Cloudant)
 // exports.db = 'https://<user>:<password>@<username>.cloudant.com/';
+
+//change them to something similar like
+exports.emailType = 'SMTP';
+exports.emailSettings = {
+  service: 'Gmail',
+  auth: {
+    user: 'paigetech@gmail.com',
+    pass: 'lbbikpivqozorgcz'
+  }
+};
