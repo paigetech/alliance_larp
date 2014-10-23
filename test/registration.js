cc74019e5844d0786bf9432f4521f462d96d0f6b
feather <paigetech@gmail.com>
2014-10-23 16:06:55 -0700

    resetting from bad tests

--- test/registration.js
+++ test/registration.js
@@ -54,6 +54,7 @@ describe('registration page', function() {
 
 
   it('should refuse empty submissions');
+
   it('should refuse partial submissions');
   it('should keep values on partial submissions');
   it('should refuse invalid emails');
